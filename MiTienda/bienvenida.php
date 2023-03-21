c3533a8299473d999eb23ee3dd29063575925a51
Amanda <amandaroblesurena@gmail.com>
2023-03-21 17:18:40 +0100

    bienvenida

--- MiTienda/bienvenida.php
+++ MiTienda/bienvenida.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
 <?php
 // inicia la sesión
 session_start();
@@ -7,3 +16,7 @@ $usuario = $_SESSION["usuario"];
 ?>
 
 <h1>Bienvenido, <?php echo $usuario; ?>!</h1>
+    
+</body>
+</html>
+
