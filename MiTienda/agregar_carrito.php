d347fc3af9aee32b505dc5e65ef568e1f3452f11
Amanda <amandaroblesurena@gmail.com>
2023-03-22 19:59:02 +0100

    yuuuu

--- MiTienda/agregar_carrito.php
+++ MiTienda/agregar_carrito.php
@@ -1,59 +1,56 @@
 <?php
 
-if(isset($_POST['1'])){
-  $id=1;
-  $nombre="MSI MPG B55 GAMING PLUS";
-  $cantidad=$_POST['cantidad'];
-  $precio=100.5;
+// Definir un array de productos con id, nombre y precio
+$productos = array(
+    1 => array(
+        'nombre' => 'MSI MPG B55 GAMING PLUS',
+        'precio' => 100.5
+    ),
+    2 => array(
+        'nombre' => 'Otro producto',
+        'precio' => 50.0
+    ),
+    3 => array(
+        'nombre' => 'Otro producto',
+        'precio' => 50.0
+    ),
+    4 => array(
+        'nombre' => 'Otro producto',
+        'precio' => 50.0
+    ),
+    5 => array(
+        'nombre' => 'Otro producto',
+        'precio' => 50.0
+    ),
+    6 => array(
+        'nombre' => 'Otro producto',
+        'precio' => 50.0
+    ),
+    7 => array(
+        'nombre' => 'Otro producto',
+        'precio' => 50.0
+    ),
+    8 => array(
+        'nombre' => 'Otro producto',
+        'precio' => 50.0
+    ),
+    9 => array(
+        'nombre' => 'Otro producto',
+        'precio' => 50.0
+    ),
+    // Agregar más productos aquí
+);
 
-  echo $precio;
+// Recorrer los productos con un bucle
+foreach($productos as $id => $producto){
+    if(isset($_POST[$id])){
+        $nombre = $producto['nombre'];
+        $cantidad = $_POST['cantidad'];
+        $precio = $producto['precio'];
+
+        echo $precio;
+        // Realizar otras acciones con los datos del producto
+        break; // Salir del bucle después de encontrar el producto
+    }
 }
-
-if(isset($_POST['2'])){
-    $id=2;
-    $nombre="MSI MPG B55 GAMING PLUS";
-    $cantidad=$_POST['cantidad'];
-    $precio=100.5;
-  }
-
-  if(isset($_POST['3'])){
-    $id=3;
-    $nombre="MSI MPG B55 GAMING PLUS";
-    $cantidad=$_POST['cantidad'];
-    $precio=100.5;
-  }
-
-  if(isset($_POST['4'])){
-
-    $nombre="MSI MPG B55 GAMING PLUS";
-    $cantidad=$_POST['cantidad'];
-    $precio=100.5;
-  }
-
-  if(isset($_POST['5'])){
-
-    $nombre="MSI MPG B55 GAMING PLUS";
-    $cantidad=$_POST['cantidad'];
-    $precio=100.5;
-  }
-
-  if(isset($_POST['1'])){
-
-    $nombre="MSI MPG B55 GAMING PLUS";
-    $cantidad=$_POST['cantidad'];
-    $precio=100.5;
-  }
-
-  if(isset($_POST['1'])){
-
-    $nombre="MSI MPG B55 GAMING PLUS";
-    $cantidad=$_POST['cantidad'];
-    $precio=100.5;
-  }
-
-  if(isset($_POST['1'])){
-
-    $nombre="MSI MPG B55 GAMING PLUS";
-    $cantidad=$_POST['cantidad'];
-    $precio=100.5;
-  }
+?>
